8e9eb936b44d2ca00ba08b5908fdc6043c53fe71
johntran <tranjohn1211@gmail.com>
2022-04-28 13:10:55 -0700

    set up file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Paper, Text, Loader, MantineProvider } from '@mantine/core';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MantineProvider theme={{
+        fontFamily: 'Open Sans',
+        colorScheme: 'dark',
+        
+      }}>
+        <Paper>
+          <Text>This is a simple text</Text>
+          <Text>This is a simple text TAG and thats all it does</Text>
+        </Paper>
+      
+        <Button>Hello world!</Button>
+        <Button>Hello world!</Button>
+        <Loader />
+        </MantineProvider>
     </div>
   );
 }
