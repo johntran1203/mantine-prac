a2367049cbe43c5573b290c72c456598f73bc8d3
johntran <tranjohn1211@gmail.com>
2022-04-28 13:29:50 -0700

    learn about mantineProvider

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,32 @@ function App() {
       <MantineProvider theme={{
         fontFamily: 'Open Sans',
         colorScheme: 'dark',
-        
-      }}>
+        fontSizes: {md: 12},
+        loader: 'bars',
+        colors: {
+          blue:  [
+            '#d5d7e0',
+            '#acaebf',
+            '#8c8fa3',
+            '#666980',
+            '#4d4f66',
+            '#34354a',
+            '#2b2c3d',
+            '#1d1e30',
+            '#0c0d21',
+            '#01010a',
+          ],
+        }
+    
+      }}
+      styles={{
+        Button: (theme) => ({
+          root: {
+            backgroundColor: theme.colors.blue[0]
+          }
+        })
+      }}
+      >
         <Paper>
           <Text>This is a simple text</Text>
           <Text>This is a simple text TAG and thats all it does</Text>
@@ -19,6 +43,11 @@ function App() {
         <Button>Hello world!</Button>
         <Button>Hello world!</Button>
         <Loader />
+
+        <MantineProvider theme={{ fontFamily: 'Greycliff CF, sans-serif' }}>
+        <Button>Greycliff CF button</Button>
+      </MantineProvider>
+      
         </MantineProvider>
     </div>
   );
